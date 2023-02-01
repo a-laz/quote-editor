548e3fdc7049fcbb99850f2b7618b9dfc86d53de
Alex Lazarev <mortaleusmarketing@gmail.com>
2023-01-31 20:24:15 -0500

    turbo_stream_channel and flash messages

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,8 @@
 class Quote < ApplicationRecord
+    belongs_to :company
     validates :name, presence: true
 
     scope :ordered, -> { order(id: :desc) }
-    after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+
+    broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
